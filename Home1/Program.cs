12aa861c281c640a1a7967c0e031896db54e69d3
alexdmitrievi <103923985+alexdmitrievi@users.noreply.github.com>
2022-06-21 20:13:40 +0600

    end

---
+++ Home1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Показать натуральные числа от M до N, N и M заданы
+Найти сумму элементов от M до N, N и M заданы*/
+
+void f(int m, int n){
+    for (int i = m; i <= n; i++){
+        Console.WriteLine("{i} ");
+    }
+}
+
+
+
+
+
+
+Console.WriteLine("Введите число m");
+int m = int.Parse(Console.ReadLine()?? "");
+Console.WriteLine("Введите число n");
+int n = int.Parse(Console.ReadLine()?? "");
+f(m,n);
+
+
---
+++ Home2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int f(int m, int n){
+    int sum = 0;
+    for (int i = m; i <= n; i++){
+        sum = sum + i;
+    }
+    return sum;
+}
+
+
+
+
+
+
+Console.WriteLine("Введите число m");
+int m = int.Parse(Console.ReadLine()?? "");
+Console.WriteLine("Введите число n");
+int n = int.Parse(Console.ReadLine()?? "");
+Console.WriteLine(f(m,n));
+
+
+
+

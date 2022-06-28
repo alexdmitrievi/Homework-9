6daf154a655cddc36aee4b9ba9c04d7d06edb57a
alexdmitrievi <103923985+alexdmitrievi@users.noreply.github.com>
2022-06-28 19:49:00 +0600

    en

--- Home2/Program.cs
+++ Home2/Program.cs
@@ -1,11 +1,10 @@
 ﻿// Найти сумму элементов от M до N, N и M заданы
 
-int f(int m, int n){
-    int sum = 0;
-    for (int i = m; i <= n; i++){
-        sum = sum + i;
+int f(int m, int n, int sum){
+    if (m == n){
+        return sum + m;
     }
-    return sum;
+    return f(m+1,n,sum+m);
 }
 
 
@@ -17,8 +16,8 @@ Console.WriteLine("Введите число m");
 int m = int.Parse(Console.ReadLine()?? "");
 Console.WriteLine("Введите число n");
 int n = int.Parse(Console.ReadLine()?? "");
-Console.WriteLine(f(m,n));
-
+int sum = 0;
+Console.WriteLine(f(m,n,sum));
 
 
 
